0d08942916b28312dc7db18579c64fd04a5215e5
marwa-p <107890767+marwa-p@users.noreply.github.com>
2022-06-21 09:25:44 +1200

    Add files via upload

---
+++ ver 0 lucky unicorn.py
@@ -0,0 +1,18 @@
+"""
+base component for the game lucky unicorn
+version 0 - skeleton setup
+
+author- Marwa Pash
+CC MP 2022
+"""
+
+# import libraries libraries below this lone ******************************
+
+
+# all functions below this line *********************************************
+
+
+# main routine below this line ***********************************************
+
+print("Hello")
+
